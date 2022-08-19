8f02d1d423570d68d5ef11b34cee7e784d265c6f
Asus <mr.akmal4555@mail.ru>
2022-08-19 16:21:10 +0500

    Initial commit

---
+++ src/main/java/com/example/financial_year_service/FinancialYearServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.financial_year_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialYearServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialYearServiceApplication.class, args);
+    }
+
+}
